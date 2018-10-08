617dc0924b05abf5d5edfe8592c6f604865cea85
Vladimir <43782921+roksanlul@users.noreply.github.com>
2018-10-09 00:17:38 +0500

    new mailto new error
    help

--- report.php
+++ report.php
@@ -7,12 +7,23 @@
    $alien_description = $_POST['aliendescription'];
    $fang_spotted = $_POST['fangspotted'];
    $email = $_POST['email'];
-$alien_description ='гуманоид из планеты жопа';
-
+   $alien_description ='гуманоид из планеты жопа';
     $name = $_POST['firstname'] . ' ' .  $_POST['lastname'];
     $how_many = $_POST['howmany'];
     $what_they_did = $_POST['whattheydid'];
     $other = $_POST['other'];
+
+
+    $to  = 'roksan98@gmail.com';
+    $subject = 'Космические пришельцы дебилы - не воруйте';
+         $msg = "$name . 'был похищен' . $when_it_happend 'и отсуствовал в течение' . $how_long. \n" . 
+      "количество космических пришельцев: ' . $how_many . \n" .
+       "Описание пришельцев: ' . $alien_description . \n" .
+       "что они делали? ' . $what_they_did . \n" .
+        "фэнг замечен? ' . $fang_spotted . \n" .
+         "Дополнительная инфа: ' . $other";
+         mail($to, $subject, $msg 'From: ' . $email);
+
 
   echo 'Спасибо за заполнение'. '<br />';
   echo 'Вы были похищены ' . $when_it_happend;
@@ -24,10 +35,8 @@ $alien_description ='гуманоид из планеты жопа';
   echo "сколько их " . $how_many . '<br />';
   echo 'че они сделали ' . $what_they_did . '<br />';
   echo  "доп информация " . $other . '<br />';
-     $msg = "$name . 'был похищен' . $when_it_happend 'и отсуствовал в течение' . $how_long . '.'
-      'количество космических пришельцев: ' . $how_many . '\n' . 
-       'Описание пришельцев: ' . $alien_description . '\n' 
-       'что они делали? ' . $what_they_did . '\n' 
-        'фэнг замечен? ' . $fang_spotted . '\n' 
-         'Дополнительная инфа: ' . $other";
+   
+  
+
+
 ?>
